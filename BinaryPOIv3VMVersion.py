64d3c4386226375bb6c471054059f5b093e5c97d
williamcheung94 <61169234+williamcheung94@users.noreply.github.com>
2020-07-06 14:49:13 -0400

    Update BinaryPOIv3VMVersion.py

--- BinaryPOIv3VMVersion.py
+++ BinaryPOIv3VMVersion.py
@@ -21,14 +21,6 @@ def preprocess2(df):
     preprocessed_df=df.drop(columns=remove_cols,axis=1)
     return preprocessed_df
 
-def select_pid(row_threshold=200):
-    all_df_pd=pd.read_csv("data-cnt-info-will.csv").iloc[:,1:]
-    #print(all_df_pd)
-    #all_df_pd = pd.read_csv("C:\\Users\\willi\\PycharmProjects\\Test\\POI_project\\0314\\Data_info.ipynb").iloc[:, 1:]
-    #all_df_pd.head()
-    pid_selected=all_df_pd.loc[all_df_pd['3']>row_threshold,"PID"] #[all_df_pd['3']>row_threshold,"PID"]
-    print(pid_selected)
-    return pid_selected
 
 def initial_balance(df, PIDs, POIs, min_data=200):
     df_balanced = pd.DataFrame(None, columns=df.columns)
@@ -169,7 +161,6 @@ def feature_select_KBest(features, Xtrain_df, Ytrain_df, Xtest_df):
     return X_train, X_test, Top_Rank
 
 
-pid_selected = select_pid(row_threshold=200)
 feature_count = 20
 pid = 100  # check if i still need this
 classes = [1, 2, 3, 4]  # check if i still need this
@@ -184,7 +175,7 @@ min_data = 200
 source_data_path = "C:\\Users\\willi\\PycharmProjects\\Test\\POI_project\\0603Data\\Clean\\DataCleaner_6_12_output"  # Will Data with general_func.preprocess2
 target_data_path = "C:\\Users\\willi\\PycharmProjects\\Test\\POI_project\\Data\\6_09"
 
-df = pd.read_csv(source_data_path + "\\bio_feat_time_4pois_pid26clean.csv")
+df = pd.read_csv("bio_feat_time_4pois_pid26clean.csv")
 for column in ["time", "index", "dayOfWeek", "isWorkDay", "snrS", "snrM", "snrC", "snrH"]:
     if (column in df.columns):
         df = df.drop([column], axis=1)
